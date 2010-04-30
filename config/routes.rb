e71c1995e696053d8acb89520c8283e023c9e973
Cyril Mougel <cyril@yeastymobs.com>
2010-04-30 18:59:28 +0200

    add menu with logged/unlogged information

--- config/routes.rb
+++ config/routes.rb
@@ -48,9 +48,9 @@ Mygithub::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
   devise_for :users
   resources :users
+  root :to => "welcome#index"
 
   # See how all your routes lay out with "rake routes"
 
